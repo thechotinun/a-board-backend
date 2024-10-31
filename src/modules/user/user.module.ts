d971c937d5cdb4931fd1512f89144d6de4c797c5
thechotinun <thechotinun@gmail.com>
2024-10-31 16:16:51 +0700

    add API to receive user-generated posts

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -4,13 +4,14 @@ import { User } from '@entities/user.entity';
 import { UserService } from '@modules/user/services/user.service';
 import { UserController } from '@modules/user/controllers/user.controller';
 import { UserRepository } from '@repositories/user.repository';
+import { PostRepository } from '@repositories/post.repository';
 import { AuthenticateMiddleware } from '@common/middlewares/auth/authenticate.middlewares';
 import { AuthModule } from '@modules/auth/auth.module';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User]), AuthModule],
   controllers: [UserController],
-  providers: [UserRepository, UserService],
+  providers: [UserRepository, UserService, PostRepository],
   exports: [UserService],
 })
 export class UserModule implements NestModule {
